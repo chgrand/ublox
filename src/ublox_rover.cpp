a1bf08b42e9d204e4adc075abf643711cf0dd5db
Christophe Grand <christophe.grand@onera.fr>
2015-08-24 15:40:36 +0200

    clean files

--- src/ublox_rover.cpp
+++ src/ublox_rover.cpp
@@ -13,59 +13,55 @@ UBX_Port ublox;
 //-----------------------------------------------------------------------------
 bool configure()
 {
-  if(!ublox.connect("/dev/ttyACM0")) {
-    printf("Error cannot connect GPS on port /dev/ttyACM0\n");
-    return false;
-  }
-  ublox.set_baudrate(B115200);
+    if(!ublox.connect("/dev/ttyACM0")) {
+        printf("Error cannot connect GPS on port /dev/ttyACM0\n");
+        return false;
+    }
+    ublox.set_baudrate(B115200);
 
-  ubx_packet_t packet;
-  
-  ublox.set_debug(true);
-  
-  // set baudrate (Not necessary on usb port)
-  uint32_t baudrate=115200;
-  uint32_t mode = (3<<6)+(4<<9)+(0<<12);
+    ubx_packet_t packet;
+    ublox.set_debug(true);
 
-  packet.id    = ubx::CFG_PRT;
-  ublox.poll_message(&packet);
-  ubx::U4_write(packet.data+4, mode);
-  ubx::U4_write(packet.data+8, baudrate);
-  packet.data[14] = protoMask_UBX;      // desactivate NMEA output  
-  ublox.write_message(&packet);
-  
-  // set GNSS freq = 10hz
-  packet.id   = ubx::CFG_RATE;
-  packet.len  = 6;
-  ubx::U2_write(packet.data+0, 100); // Gnss measurement rate in ms
-  ubx::U2_write(packet.data+2, 1);   // Navigation Rate, in number of measurement cycles == 1
-  ubx::U2_write(packet.data+4, 1);   // Alignment to reference time (0: UTC time, 1: GPS time)
-  ublox.write_message(&packet);
-  
-  // Set GNSS Nav properties
-  printf("NAV5-------------\n");
-  packet.id    = ubx::CFG_NAV5;
-  ublox.poll_message(&packet);
-  packet.data[2] = 2;              // dynModel = 2 (stationnary)
-  ublox.write_message(&packet);
+    // set baudrate (Not necessary on usb port)
+    uint32_t baudrate=115200;
+    uint32_t mode = (3<<6)+(4<<9)+(0<<12);
 
-  //ublox.print(packet);
+    packet.id    = ubx::CFG_PRT;
+    ublox.poll_message(&packet);
+    ubx::U4_write(packet.data+4, mode);
+    ubx::U4_write(packet.data+8, baudrate);
+    packet.data[14] = protoMask_UBX;      // desactivate NMEA output
+    ublox.write_message(&packet);
 
+    // set GNSS freq = 10hz
+    packet.id   = ubx::CFG_RATE;
+    packet.len  = 6;
+    ubx::U2_write(packet.data+0, 100); // Gnss measurement rate in ms
+    ubx::U2_write(packet.data+2, 1);   // Nav. Rate, in number of meas. cycles
+    ubx::U2_write(packet.data+4, 1);   // Ref. time (0: UTC time, 1: GPS time)
+    ublox.write_message(&packet);
 
-  // set UBX_MSG active
-  packet.id    = ubx::CFG_MSG;
-  packet.len  = 3;
-  ubx::U2_write(packet.data, ubx::NAV_POSECEF);  // Position in ECEF
-  packet.data[2] = 0;                            // At each cycle 
-  ublox.write_message(&packet);
+    // Set GNSS Nav properties
+    printf("NAV5-------------\n");
+    packet.id    = ubx::CFG_NAV5;
+    ublox.poll_message(&packet);
+    packet.data[2] = 2;              // dynModel = 2 (stationnary)
+    ublox.write_message(&packet);
 
-  ubx::U2_write(packet.data, ubx::NAV_POSLLH);  // Position in Lat-Long
-  packet.data[2] = 1;                            // At each cycle 
-  ublox.write_message(&packet);
+    // Select active UBX_MSG
+    packet.id    = ubx::CFG_MSG;
+    packet.len  = 3;
+    ubx::U2_write(packet.data, ubx::NAV_POSECEF);  // Position in ECEF
+    packet.data[2] = 0;                            // At each cycle
+    ublox.write_message(&packet);
 
-  ublox.set_debug(false);
+    ubx::U2_write(packet.data, ubx::NAV_POSLLH);  // Position in Lat-Long
+    packet.data[2] = 1;                            // At each cycle
+    ublox.write_message(&packet);
 
-  return true;
+    ublox.set_debug(false);
+
+    return true;
 
 }
 
@@ -74,44 +70,44 @@ bool configure()
 int process_message()
 // return ID of received message
 {
-  ubx_packet_t packet;
+    ubx_packet_t packet;
 
-  if(ublox.wait_sync_timeout(10)) { // 10ms timeout
-    ublox.read_packet(&packet);
-    
-    switch(packet.id) {
-    case ubx::NAV_POSECEF:
-      printf("[%i ms]: NAV_POSECEF = ", ubx::U4_read(packet.data));
-      printf("%.3f ", (double)ubx::I4_read(packet.data+4)/100.); 
-      printf("%.3f ", (double)ubx::I4_read(packet.data+8)/100.); 
-      printf("%.3f ", (double)ubx::I4_read(packet.data+12)/100.); 
-      printf("(%.3f)", (double)ubx::I4_read(packet.data+16)/100.); 
-      break;
+    if(ublox.wait_sync_timeout(10)) { // 10ms timeout
+        ublox.read_packet(&packet);
 
-    case ubx::NAV_POSLLH:
-      printf("[%i ms]: NAV_POSLLH = ", ubx::U4_read(packet.data));
-      printf("%.8f ", (double)ubx::I4_read(packet.data+4)*1e-7); 
-      printf("%.8f ", (double)ubx::I4_read(packet.data+8)*1e-7); 
-      printf("%.4f ", (double)ubx::I4_read(packet.data+12)/1000.); 
-      printf("(%.4f)", (double)ubx::I4_read(packet.data+20)/1000.); 
-      break;
+        switch(packet.id) {
+        case ubx::NAV_POSECEF:
+            printf("[%i ms]: NAV_POSECEF = ", ubx::U4_read(packet.data));
+            printf("%.3f ", (double)ubx::I4_read(packet.data+4)/100.);
+            printf("%.3f ", (double)ubx::I4_read(packet.data+8)/100.);
+            printf("%.3f ", (double)ubx::I4_read(packet.data+12)/100.);
+            printf("(%.3f)", (double)ubx::I4_read(packet.data+16)/100.);
+            break;
 
-    default:
-      printf("Unknown id = %0x4x", packet.id);
+        case ubx::NAV_POSLLH:
+            printf("[%i ms]: NAV_POSLLH = ", ubx::U4_read(packet.data));
+            printf("%.8f ", (double)ubx::I4_read(packet.data+4)*1e-7);
+            printf("%.8f ", (double)ubx::I4_read(packet.data+8)*1e-7);
+            printf("%.4f ", (double)ubx::I4_read(packet.data+12)/1000.);
+            printf("(%.4f)", (double)ubx::I4_read(packet.data+20)/1000.);
+            break;
+
+        default:
+            printf("Unknown id = %0x4x", packet.id);
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
 }
 
 
 //-----------------------------------------------------------------------------
 int main(void)
 {
-  if(!configure())
-    return -1;
+    if(!configure())
+        return -1;
 
-  while(1)
-    process_message();
+    while(1)
+        process_message();
 
-  return 0;
+    return 0;
 }
--- src/ubx_protocol.h
+++ src/ubx_protocol.h
@@ -15,82 +15,70 @@ const uint8_t SYNC[2] = {0xB5, 0x62};
 
 typedef struct
 {
-  uint16_t id;
-  uint16_t len;
-  uint8_t  data[MAX_DATA_LEN];
-} 
-ubx_packet_t;
+    uint16_t id;
+    uint16_t len;
+    uint8_t  data[MAX_DATA_LEN];
+}
+    ubx_packet_t;
 
 namespace ubx
 {
-  const static uint16_t CFG_PRT  = 0x0006;
-  const static uint16_t CFG_MSG  = 0x0106;
-  const static uint16_t CFG_RST  = 0x0406;
-  const static uint16_t CFG_RATE = 0x0806;
-  const static uint16_t CFG_NMEA = 0x1706;
-  const static uint16_t CFG_NAV5 = 0x2406;
+    const static uint16_t CFG_PRT  = 0x0006;
+    const static uint16_t CFG_MSG  = 0x0106;
+    const static uint16_t CFG_RST  = 0x0406;
+    const static uint16_t CFG_RATE = 0x0806;
+    const static uint16_t CFG_NMEA = 0x1706;
+    const static uint16_t CFG_NAV5 = 0x2406;
 
-  const static uint16_t NAV_POSECEF = 0x0101;
-  const static uint16_t NAV_POSLLH  = 0x0201;
-  
- 
-  inline void U4_write(uint8_t* buf, uint32_t value) 
-  {
-    memcpy(buf, &value, 4);
-  }
+    const static uint16_t NAV_POSECEF = 0x0101;
+    const static uint16_t NAV_POSLLH  = 0x0201;
 
-  inline void U2_write(uint8_t* buf, uint16_t value)
-  {
-    memcpy(buf, &value, 2);
-  }
+    inline void U4_write(uint8_t* buf, uint32_t value) { memcpy(buf, &value, 4);};
+    inline void U2_write(uint8_t* buf, uint16_t value) { memcpy(buf, &value, 2);};
+    inline void U1_write(uint8_t* buf, uint8_t value) { memcpy(buf, &value, 1);};
+    inline void I4_write(uint8_t* buf, int32_t value) { memcpy(buf, &value, 4);};
+    inline void I2_write(uint8_t* buf, int16_t value) { memcpy(buf, &value, 2);};
+    inline void I1_write(uint8_t* buf, int8_t value) { memcpy(buf, &value, 1);};
 
-  inline uint32_t U4_read(uint8_t* buf)
-  {
-    uint32_t value;
-    memcpy(&value, buf,4);
-    return value;
-  }
-
-  inline int32_t I4_read(uint8_t* buf)
-  {
-    int32_t value;
-    memcpy(&value, buf,4);
-    return value;
-  }
+    inline uint32_t U4_read(uint8_t* buf) { uint32_t value; memcpy(&value, buf,4); return value;};
+    inline uint16_t U2_read(uint8_t* buf) { uint16_t value; memcpy(&value, buf,2); return value;};
+    inline uint8_t U1_read(uint8_t* buf) { uint8_t value; memcpy(&value, buf,1); return value;};
+    inline int32_t I4_read(uint8_t* buf) { int32_t value; memcpy(&value, buf,4); return value;};
+    inline int16_t I2_read(uint8_t* buf) { int16_t value; memcpy(&value, buf,2); return value;};
+    inline int8_t I1_read(uint8_t* buf) { int8_t value; memcpy(&value, buf,1); return value;};
 };
 
 
 class UBX_Port
 {
 public:
-  UBX_Port() : _connected(false), _debug(false) {};
-  ~UBX_Port();
+    UBX_Port() : _connected(false), _debug(false) {};
+    ~UBX_Port();
 
 
-  // serial port configuration and connection
-  bool connect(const char *device);
-  void disconnect();
-  bool isConnected() {return _connected;}
-  void set_baudrate(speed_t speed);
-  void set_debug(bool flag) {_debug = flag;}
+    // serial port configuration and connection
+    bool connect(const char *device);
+    void disconnect();
+    bool isConnected() {return _connected;}
+    void set_baudrate(speed_t speed);
+    void set_debug(bool flag) {_debug = flag;}
  
-  // low-level function read/write packet
-  bool read_packet(ubx_packet_t *packet);
-  void write_packet(ubx_packet_t *packet);
-  bool wait_sync();
-  bool wait_sync_timeout(int timeout_ms);
-  bool wait_ack();
+    // low-level function read/write packet
+    bool read_packet(ubx_packet_t *packet);
+    void write_packet(const ubx_packet_t *packet);
+    bool wait_sync();
+    bool wait_sync_timeout(int timeout_ms);
+    bool wait_ack();
+    void print_packet(const ubx_packet_t *packet);
 
-  // hi-level function
-  bool poll_message(ubx_packet_t *packet);
-  bool write_message(ubx_packet_t *packet);
-  //bool read_message(ubx_packet_t *packet, int timeout=0);  // timeout in 10th of ms
+    // hi-level function
+    bool poll_message(ubx_packet_t *packet);
+    bool write_message(ubx_packet_t *packet);
 
 private:
-  int _serial_fd;
-  bool _connected;
-  bool _debug;
+    int _serial_fd;
+    bool _connected;
+    bool _debug;
 };
-
 
 #endif
